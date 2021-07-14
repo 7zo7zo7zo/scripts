b579ea6adaf6c7bd661f741abf09559361949285
7zo7zo7zo <7zo7zo7zo@gmail.com>
2021-07-14 08:13:25 -0700

    add hash script

---
+++ hash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+filename=$1
+printf "Filename: ./debs/" ; basename $filename ;
+printf "Size: " ; ls -l $filename | cut -d' ' -f5 ;
+printf "MD5sum: " ; md5sum $filename | cut -d' ' -f1 ;
+printf "SHA1: " ; sha1sum $filename | cut -d' ' -f1 ;
+printf "SHA256: " ; sha256sum $filename | cut -d' ' -f1 ;
+printf "SHA512: " ; sha512sum $filename | cut -d' ' -f1 ;
