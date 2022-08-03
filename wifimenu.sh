659e1e9c1b300dc18f082a1942174f07b9489627
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-08-03 14:13:05 -0700

    Update wifimenu.sh

--- wifimenu.sh
+++ wifimenu.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+
 bssid=$(nmcli device wifi list | sed -n  '1!p' | cut -b 9- | dmenu -p "Select WiFi: " -l 20 | cut -d' ' -f1)
 pass=$(printf "" | dmenu -p "Enter password: ")
 nmcli device wifi connect $bssid password $pass
