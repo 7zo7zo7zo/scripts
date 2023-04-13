fabce239d432b8fa669b3538b0032f05e1db7756
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2023-04-12 23:00:15 -0700

    Update hash.sh

--- hash.sh
+++ hash.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-filename=$1
-printf "Filename: ./debs/" ; basename $filename
-printf "Size: " ; ls -l $filename | cut -d' ' -f5
-printf "MD5sum: " ; md5sum $filename | cut -d' ' -f1
-printf "SHA1: " ; sha1sum $filename | cut -d' ' -f1
-printf "SHA256: " ; sha256sum $filename | cut -d' ' -f1
-printf "SHA512: " ; sha512sum $filename | cut -d' ' -f1
+printf "Filename: ./debs/" ; basename $1
+printf "Size: " ; ls -l $1 | cut -d' ' -f5
+printf "MD5sum: " ; md5sum $1 | cut -d' ' -f1
+printf "SHA1: " ; sha1sum $1 | cut -d' ' -f1
+printf "SHA256: " ; sha256sum $1 | cut -d' ' -f1
+printf "SHA512: " ; sha512sum $1 | cut -d' ' -f1
